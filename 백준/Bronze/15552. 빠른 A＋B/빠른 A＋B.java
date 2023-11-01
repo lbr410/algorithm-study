97cc9ef6def0902f5bebc637bcd302dc4c2253c8
devBB <lbor3280@naver.com>
2023-11-02 05:09:17 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 860 ms, Memory: 236280 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int count = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=count; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int num1 = Integer.parseInt(st.nextToken());
+			int num2 = Integer.parseInt(st.nextToken());
+			bw.write(num1+num2+"\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
