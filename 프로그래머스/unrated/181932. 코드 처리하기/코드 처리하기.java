a05ef707005aa21b62db2606e04f77b8754f5f73
devBB <lbor3280@naver.com>
2023-11-28 15:59:17 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        char arr[] = new char[code.length()];
+        int mode = 0;
+        
+        for(int i=0; i<arr.length; i++) {
+        	if(code.charAt(i) == '1') {
+        		if(mode == 0) {
+        			mode = 1;
+        		} else {
+        			mode = 0;
+        		}
+        	}
+        	if(mode == 0) {
+        		if(i%2 == 0) {
+        			if(code.charAt(i) != '1') {
+        				answer = answer+code.charAt(i);
+        			}
+        		}
+        	} else {
+        		if(i%2 == 1) {
+        			if(code.charAt(i) != '1') {        				
+        				answer = answer+code.charAt(i);
+        			}
+        		}
+        	}
+        	arr[i] = code.charAt(i);
+        }
+        
+        if(answer.equals("")) {
+        	answer = "EMPTY";
+        }
+
+        return answer;
+    }
+}
