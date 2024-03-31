bed5772fecea304b2c9dddc60b5abd5378d0f0c0
devBB <lbor3280@naver.com>
2024-04-01 04:49:38 +0900

    [Bronze V] Title: 합, Time: 200 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int res = 0;
+        for(int i=1; i<=n; i++) {
+            res = res+i;
+        }
+        System.out.println(res);
+	}
+}
