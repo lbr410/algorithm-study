c09fc58e9cd3d5bd36e26930e1a4b3383af3e2e6
devBB <lbor3280@naver.com>
2024-04-01 04:34:49 +0900

    [Bronze IV] Title: 주사위 세개, Time: 204 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,30 +1,28 @@
-import java.util.*;
+import java.util.Scanner;
 
 class Main {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		
-		if(a==b && b==c && a==c) {
-			System.out.println(10000+(a*1000));
-		} else if(a!=b && b==c && a!=c) {
-			System.out.println(1000+(b*100));
-		} else if(a!=b && b!=c && a==c) {
-			System.out.println(1000+(c*100));
-		} else if(a==b && b!=c && a!=c) {
-			System.out.println(1000+(a*100));
-		} else if(a!=b && b!=c && a!=c) {
-			if(a>b && a>c) {
-				System.out.println(a*100);
-			} else if(b>a && b>c) {
-				System.out.println(b*100);
-			} else if(c>a && c>b) {
-				System.out.println(c*100);
-			}
-		}
+        
+        int i = sc.nextInt();
+        int j = sc.nextInt();
+        int k = sc.nextInt();
+        int res = 0;
+        
+        if(i==j && j==k) {
+            res = 10000+(i*1000);
+        } else if(i==j || j==k || i==k) {
+            if(i==j || i==k) {
+                res = 1000+(i*100);
+            } else {
+                res = 1000+(j*100);
+            }
+        } else if(i!=j && j!=k && i!=k) {
+            int max = 0;
+            max = i>j ? i : j;
+            max = max>k ? max : k;
+            res = max*100;
+        }
+        System.out.println(res);
 	}
 }
