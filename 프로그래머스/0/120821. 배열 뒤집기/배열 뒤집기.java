f758b4b7457f64bb8e64cf5606cc4e66229d4950
devBB <lbor3280@naver.com>
2024-11-04 16:39:24 +0900

    [level 0] Title: 배열 뒤집기, Time: 3.44 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,17 +1,8 @@
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        List<Integer> list = Arrays.stream(num_list)
-                            .boxed()
-                            .collect(Collectors.toList());
-        Collections.reverse(list);
-        int[] answer = new int[list.size()];
-        for (int i=0; i<list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-        
-        return answer;
+        return IntStream.rangeClosed(1, num_list.length)
+                        .map(i -> num_list[num_list.length - i]).toArray();
     }
 }
